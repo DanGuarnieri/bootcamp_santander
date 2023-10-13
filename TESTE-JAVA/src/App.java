92a328b478c0cf5bc54388614e7a3068a721a3fa
DanGuarnieri <daniloguarnieri@outlook.com>
2023-10-13 00:52:36 -0300

    Projeto de Validação de processo seletivo

---
+++ TESTE-JAVA/src/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        boolean condicao = false;
+
+        while(condicao){
+            System.out.println("executou ... ");
+        }
+    }
+
+}
